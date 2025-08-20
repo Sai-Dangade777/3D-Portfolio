44ff625f2c23b1e1fe9da4e0e5d1da1ef2302dcf
Sai-Dangade777 <sai3dangade@gmail.com>
2025-08-20 11:49:33 +0530

    Hide Firebase API key using .env and update .gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0ty8xuoTh4BDAxFf3WQr2MMhxoP2xRus",
-  authDomain: "portfolio-4dd55.firebaseapp.com",
-  projectId: "portfolio-4dd55",
-  storageBucket: "portfolio-4dd55.firebasestorage.app",
-  messagingSenderId: "406539529760",
-  appId: "1:406539529760:web:811c79841051ad2a5f5645",
-  measurementId: "G-T9M76ES15M"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
